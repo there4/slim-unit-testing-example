ac2afb0f9665163c8a2a1af366dc2db8862c805c
Jeremy Kendall <jkendall@opensky.com>
2014-03-05 10:50:09 -0600

    Adds Cookie test

---
+++ tests/unit/There4/Tests/Authentication/CookieTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace There4\Tests\Authentication;
+
+use There4\Authentication\Cookie;
+
+class CookieTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAuthenticate()
+    {
+        $cookie = new Cookie();
+        $this->assertFalse($cookie->authenticate('token'));
+    }
+}

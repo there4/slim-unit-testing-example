b29bdfb4cb59d35196e1a30a7311bf2607d1aab5
Craig Davis <craig@there4development.com>
2014-03-17 14:09:05 -0500

    Initial commit with new slim-test-helpers dependency
    Temporarily remove all but the file store tests so that we can isolate
    troubles as we implement the new client/app bootstrap split.

--- tests/integration/FileStoreTest.php
+++ tests/integration/FileStoreTest.php
@@ -1,9 +1,25 @@
 <?php
 
-class FileStoreTest extends Slim_Framework_TestCase
+use There4\Slim\Test\WebTestCase;
+
+class FileStoreTest extends WebTestCase
 {
     const AUTH_PASS = true;
     const AUTH_FAIL = false;
+
+    public function getSlimInstance() {
+        // Initialize our own copy of the slim application
+        $app = new \Slim\Slim(array(
+            'version'        => '0.0.0',
+            'debug'          => false,
+            'mode'           => 'testing',
+            'templates.path' => __DIR__ . '/../app/templates'
+        ));
+
+        // Include our core application file
+        require PROJECT_ROOT . '/app/app.php';
+        return $app;
+    }
 
     private function setAuthenticationMock($passFail)
     {
@@ -17,35 +33,35 @@ class FileStoreTest extends Slim_Framework_TestCase
     public function testAuthenticationFailureGets401()
     {
         $this->setAuthenticationMock(self::AUTH_FAIL);
-        $this->get('/files/sample.json');
-        $this->assertEquals(401, $this->response->status());
+        $this->client->get('/files/sample.json');
+        $this->assertEquals(401, $this->client->response->status());
     }
 
     public function testCanDownloadFile()
     {
         $this->setAuthenticationMock(self::AUTH_PASS);
         $expected = file_get_contents(__DIR__ . '/../../file_store/sample.json');
-        $this->get('/files/sample.json');
-        $this->assertEquals(200, $this->response->status());
-        $this->assertEquals('application/json', $this->response['Content-Type']);
-        $this->assertEquals($expected, $this->response->body());
+        $this->client->get('/files/sample.json');
+        $this->assertEquals(200, $this->client->response->status());
+        $this->assertEquals('application/json', $this->client->response['Content-Type']);
+        $this->assertEquals($expected, $this->client->response->body());
     }
 
     public function testMissingFileGets404()
     {
         $this->setAuthenticationMock(self::AUTH_PASS);
-        $this->get('/files/four04.json');
-        $this->assertEquals(404, $this->response->status());
+        $this->client->get('/files/four04.json');
+        $this->assertEquals(404, $this->client->response->status());
     }
 
     public function testUnknownFileTypeGetsCorectHeader()
     {
         $this->setAuthenticationMock(self::AUTH_PASS);
         $expected = file_get_contents(__DIR__ . '/../../file_store/unknownfile.type');
-        $this->get('/files/unknownfile.type');
-        $this->assertEquals(200, $this->response->status());
-        $this->assertEquals('application/octet-stream', $this->response['Content-Type']);
-        $this->assertEquals($expected, $this->response->body());
+        $this->client->get('/files/unknownfile.type');
+        $this->assertEquals(200, $this->client->response->status());
+        $this->assertEquals('application/octet-stream', $this->client->response['Content-Type']);
+        $this->assertEquals($expected, $this->client->response->body());
     }
 }
 
